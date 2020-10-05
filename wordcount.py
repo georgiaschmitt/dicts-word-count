142528b0656c24ed3808d1152c4ba4ca77848dac
vagrant <vagrant@vagrant.vm>
2020-10-05 21:44:05 +0000

    Punctuation strip

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,26 @@
-def count_words(filename):
+import sys
+from collections import Counter
+
+def count_words():
     """Return dictionary of word counts in the passed file."""
-    file_to_count = open(filename)
+    
     word_counts = {}
-    for line in file_to_count:
+    filename = sys.argv[1]
+
+    for line in open(filename):
         line = line.rstrip()
         words = line.split(" ")
         words = [word.lower() for word in words]
         for word in words:
-            if word[-1] in (",", ".", "?"):
-                word = word[:-1]
+            word = word.strip(''''/",.!?-#$%^&();:_''')
             word_counts[word] = word_counts.get(word, 0) + 1
 
     for word, count in sorted(word_counts.items()):
         print(word, count)
 
-count_words("test.txt")
-#count_words("twain.txt")
+
+count_words()
+
+
+
+
